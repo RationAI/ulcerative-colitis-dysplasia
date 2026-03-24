4cea1aa56125ab770a61a4dc2e788f20420798f6
Šimon Bořil <519192@mail.muni.cz>
2026-03-24 15:35:44 +0000

    chore: open PR

--- preprocessing/tissue_masks.py
+++ preprocessing/tissue_masks.py
@@ -1,13 +1,14 @@
-import ray
-import pandas as pd
-import pyvips
+import tempfile
 from pathlib import Path
 from typing import cast
 
 import hydra
+import mlflow
+import pandas as pd
+import pyvips
+import ray
 from omegaconf import DictConfig
 from openslide import OpenSlide
-
 from rationai.masks import (
     process_items,
     slide_resolution,
@@ -20,15 +21,9 @@ from rationai.mlkit.lightning.loggers import MLFlowLogger
 
 @ray.remote(memory=4 * 1024**3)
 def process_slide(slide_path: str, level: int, output_path: Path) -> None:
-    """Generates a tissue mask from a local slide and saves it locally."""
     slide_name = Path(slide_path).stem
     mask_file_name = f"{slide_name}.tiff"
     mask_path = output_path / mask_file_name
-
-    # Check if slide actually exists locally before opening
-    if not Path(slide_path).exists():
-        print(f"❌ Slide not found: {slide_path}")
-        return
 
     with OpenSlide(slide_path) as slide:
         mpp_x, mpp_y = slide_resolution(slide, level)
@@ -39,27 +34,29 @@ def process_slide(slide_path: str, level: int, output_path: Path) -> None:
     write_big_tiff(mask, path=str(mask_path), mpp_x=mpp_x, mpp_y=mpp_y)
 
 
-@with_cli_args([])
-@hydra.main(config_path="conf", config_name="default", version_base=None)
+@with_cli_args(["+preprocessing=tissue_masks"])
+@hydra.main(config_path="../configs", config_name="preprocessing", version_base=None)
 @autolog
 def main(config: DictConfig, logger: MLFlowLogger) -> None:
-    csv_input_path = Path(config.dataset.local_path)
-    dataset = pd.read_csv(csv_input_path)
-
-    local_mask_dir = Path(config.tissue_mask.output_dir)
-    local_mask_dir.mkdir(parents=True, exist_ok=True)
-
-    process_items(
-        dataset["slide_path"],
-        process_item=process_slide,
-        fn_kwargs={
-            "level": config.tissue_mask.level,
-            "output_path": local_mask_dir,
-        },
-        max_concurrent=config.tissue_mask.max_concurrent,
+    dataset_path = Path(
+        mlflow.artifacts.download_artifacts(artifact_uri=config.dataset_uri)
     )
+    dataset = pd.read_csv(dataset_path)
 
-    # logger.log_artifacts(local_dir=str(local_mask_dir), artifact_path="tissue_masks")
+    with tempfile.TemporaryDirectory() as tmpdir:
+        tmpdir_path = Path(tmpdir)
+
+        process_items(
+            dataset["slide_path"],
+            process_item=process_slide,  # type: ignore[arg-type]
+            fn_kwargs={
+                "level": config.level,
+                "output_path": tmpdir_path,
+            },
+            max_concurrent=config.max_concurrent,
+        )
+
+        logger.log_artifacts(tmpdir_path, "tissue_masks")
 
 
 if __name__ == "__main__":
