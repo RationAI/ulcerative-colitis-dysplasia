5b302aad8c1ca9ffd5ffe82dda918f5725e24d0c
Šimon Bořil <519192@mail.muni.cz>
2026-03-24 16:20:32 +0000

    chore: checkpoint

--- preprocessing/quality_control.py
+++ preprocessing/quality_control.py
@@ -2,7 +2,7 @@ import asyncio
 from collections.abc import Generator
 from pathlib import Path
 from typing import TypedDict
-
+import mlflow
 import hydra
 import pandas as pd
 import rationai
@@ -12,6 +12,7 @@ from rationai.mlkit import autolog, with_cli_args
 from rationai.mlkit.lightning.loggers import MLFlowLogger
 from rationai.types import SlideCheckConfig
 from tqdm.asyncio import tqdm
+import tempfile
 
 
 class QCParameters(TypedDict):
@@ -54,8 +55,9 @@ async def qc_main(
     max_concurrent: int,
     qc_parameters: QCParameters,
 ) -> None:
-    slides = slides[0]
-    async with rationai.AsyncClient() as client:  
+    slides = list(slides[0:2])
+    print("start")
+    async with rationai.AsyncClient() as client:
         async for result in tqdm(
             client.qc.check_slides(
                 slides,
@@ -66,11 +68,14 @@ async def qc_main(
             ),
             total=len(slides),
         ):
+            print("progress")
             if not result.success:
+                print("CHYBA!!!")
                 with open(output_path / "qc_errors.log", "a") as log_file:
                     log_file.write(
                         f"Failed to process {result.wsi_path}: {result.error}\n"
                     )
+                    print(f"Failed to process {result.wsi_path}: {result.error}\n")
 
         for prefix, artifact_name in get_qc_masks(qc_parameters):
             organize_masks(Path(output_path), artifact_name, prefix)
@@ -83,28 +88,30 @@ async def qc_main(
         for f in csvs:
             f.unlink()
 
-        logger.log_artifacts(local_dir=str(output_path))
-
 
 @with_cli_args(["+preprocessing=qc"])
 @hydra.main(config_path="../configs", config_name="preprocessing", version_base=None)
 @autolog
 def main(config: DictConfig, logger: MLFlowLogger) -> None:
-    dataset = pd.read_csv(config.dataset.local_path)
-    
-    output_path = Path(config.qc_output_dir)
-    output_path.mkdir(parents=True, exist_ok=True)
+    dataset_path = Path(download_artifacts(artifact_uri=config.dataset_uri))
+    dataset = pd.read_csv(dataset_path)
 
-    asyncio.run(
-        qc_main(
-            output_path=output_path,
-            slides=dataset["slide_path"].to_list(), 
-            logger=logger,
-            request_timeout=config.request_timeout,
-            max_concurrent=config.qc_max_concurrent,
-            qc_parameters=config.qc_parameters,
+    with tempfile.TemporaryDirectory() as tmpdir:
+        tmpdir_path = Path(tmpdir)
+
+        asyncio.run(
+            qc_main(
+                output_path=tmpdir_path,
+                slides=dataset["slide_path"].to_list(),
+                logger=logger,
+                request_timeout=config.request_timeout,
+                max_concurrent=config.max_concurrent,
+                qc_parameters=config.qc_parameters,
+            )
         )
-    )
+
+        logger.log_artifacts(str(tmpdir_path), "qc_output")
+
 
 if __name__ == "__main__":
     main()
