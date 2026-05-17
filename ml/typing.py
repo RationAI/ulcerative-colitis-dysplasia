10f66fb47c60c53c9041586b6ea78a7f3f6471a9
Adam Kukučka <xkukuck1@fi.muni.cz>
2026-05-17 17:52:36 +0000

    feat: dataset tiles

--- ml/typing.py
+++ ml/typing.py
@@ -1,5 +1,32 @@
-type Sample = ...  # TODO define the type returned by Dataset
+from typing import TypedDict
 
-type Input = ...  # TODO define the model input type
+from torch import Tensor
 
-type Outputs = ...  # TODO define the model output type
+
+class Metadata(TypedDict):
+    slide_name: str
+
+
+class MetadataBatch(TypedDict):
+    slide_name: list[str]
+
+
+type Sample = tuple[Tensor, Tensor, Metadata]
+type PredictSample = tuple[Tensor, Metadata]
+
+
+class MetadataTiles(Metadata):
+    x: int
+    y: int
+
+
+class MetadataTilesBatch(MetadataBatch):
+    x: Tensor
+    y: Tensor
+
+
+type TilesSample = tuple[Tensor, Tensor, MetadataTiles]
+type TilesPredictSample = tuple[Tensor, MetadataTiles]
+
+type TilesInput = tuple[Tensor, Tensor, MetadataTilesBatch]
+type TilesPredictInput = tuple[Tensor, MetadataTilesBatch]
